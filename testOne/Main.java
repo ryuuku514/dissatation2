399bf35f05c643e25183f40ee54af3d8e5ad6c82
ryuuku514 <30598598+ryuuku514@users.noreply.github.com>
2017-08-24 06:28:03 +0100

    diss work
    calculate tau and sort

---
+++ testOne/Main.java
@@ -0,0 +1,54 @@
+package testOne;
+
+import java.awt.List;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+	
+		//generating tuple objects 
+		//list<Tuple>;
+		int x = 0;
+		int y;
+			do  { x = x + 1;
+				y = 0;
+				do { y = y + 1; 
+					if (y < 11) {
+					ArrayList<Tuple>	list = new ArrayList<Tuple>();
+			
+					}else {
+				
+					}
+			
+				}while (y < 10);
+				
+			}while(x == 10 && y == 10);
+	
+	//test1.printTuple(); 
+			
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+}
+
+
